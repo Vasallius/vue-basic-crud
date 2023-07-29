52b1b2233c190914799d2ee92fe32d9785f573e7
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-07-29 18:31:08 +0800

    feat: add authentication

--- todov3/src/App.vue
+++ todov3/src/App.vue
@@ -1,32 +1,48 @@
 <template>
+  <div v-if="isAuthenticated">
+    <div>
+      <input type="text" name="addTodo" v-model="todo" placeholder="add new todo.." />
+      <button @click="addTodo">Add</button>
+    </div>
+    <div v-for="todoItem in todos" :key="todoItem.id">
+      <button @click="editTodo(todoItem)">Edit</button>
+      <button @click="removeTodo(todoItem.id)">Clear Todo</button>
+      <input v-if="editingTodo === todoItem.id" type="text" v-model="todoItem.text" />
+      <span v-else>{{ todoItem.text }}</span>
+      <button v-if="editingTodo === todoItem.id" @click="updateTodo">Save</button>
+    </div>
+    <div>TEST</div>
+    <div>{{ data }}</div>
+    <button @click="logoutuser">LOG OUT</button>
+  </div>
+  <div v-else>
+    <button @click="login">LOGIN</button>
+  </div>
   <div>
-    <input type="text" name="addTodo" v-model="todo" placeholder="add new todo.." />
-    <button @click="addTodo">Add</button>
+    {{ user }}
   </div>
-  <div v-for="todoItem in todos" :key="todoItem.id">
-    <button @click="editTodo(todoItem)">Edit</button>
-    <button @click="removeTodo(todoItem.id)">Clear Todo</button>
-    <input v-if="editingTodo === todoItem.id" type="text" v-model="todoItem.text" />
-    <span v-else>{{ todoItem.text }}</span>
-    <button v-if="editingTodo === todoItem.id" @click="updateTodo">Save</button>
-  </div>
-  <div>TEST</div>
-  <div>{{ data }}</div>
-  <button @click="login">LOGIN</button>
 </template>
 
 <script setup>
 import { useAuth0 } from '@auth0/auth0-vue'
-import { onMounted, ref } from 'vue'
-const { loginWithRedirect } = useAuth0()
-loginWithRedirect()
-const data = ref('lol')
-const fetchData = async () => {
-  const response = await fetch('http://localhost:3000/')
-  data.value = await response.json()
-  console.log(data.value)
+import { ref } from 'vue'
+// const { loginWithRedirect } = useAuth0()
+const { loginWithPopup, user, isAuthenticated, logout } = useAuth0()
+
+const login = () => {
+  loginWithPopup()
 }
-onMounted(fetchData)
+
+const logoutuser = () => {
+  logout()
+}
+// const data = ref('lol')
+// const fetchData = async () => {
+//   const response = await fetch('http://localhost:3000/')
+//   data.value = await response.json()
+//   console.log(data.value)
+// }
+// onMounted(fetchData)
 
 const todo = ref('')
 const todos = ref([
